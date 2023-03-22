be972b26339764be0a2a41abe1d8d1dff8799b26
MilkyProject <125369423+MilkyProject@users.noreply.github.com>
2023-03-22 14:34:09 +0200

    Add files via upload

---
+++ wwwroot/js/gradient.js
@@ -0,0 +1,106 @@
+﻿let colors = 2;
+
+generateGradient();
+
+$( document ).ready(function (){
+    Coloris({themeMode: 'dark', theme: 'polaroid',defaultColor: '#f00', alpha: false, format: 'hex'});
+});
+
+document.addEventListener('coloris:pick', _ => {
+    generateGradient();
+});
+
+$("#add-btn").click(function (){
+    console.log(1);
+    $("#color-list").append("<input value='#000000' type=\"text\" class=\"color-input\" id=\"color-" + colors + "\" data-coloris>");
+    colors++;
+    updateColoris();
+    generateGradient();
+});
+
+$("#remove-btn").click(function (){
+    console.log(2);
+    if(colors === 2)
+        return;
+
+    $("#color-list").children().last().remove();
+    colors--;
+    generateGradient();
+});
+
+function updateColoris() {
+    Coloris.wrap("[data-coloris]");
+}
+
+function getAllColors() {
+    let ret = [];
+    
+    for(let i = 0; i < colors; i++){
+        ret.push($("#color-" + i).val());
+    }
+    
+    return ret;
+}
+
+function generateGradient(){
+    let word = $("#input").val();
+    
+    if(word.length < 2){
+        Swal.fire({
+            icon: 'error',
+            title: 'Input not long enough, it needs at least 2 characters.',
+            showConfirmButton: false,
+            timer: 1500
+        });
+        return;
+    }
+    
+    let rainbow = new Rainbow();
+    rainbow.setNumberRange(1, word.length);
+    rainbow.setSpectrumByArray(getAllColors());
+
+    let ret = '';
+    let copy = '';
+    
+    for(let i = 1; i <= word.length; i++){
+        let char = word.at(i - 1);
+        
+        if(char === '\n'){
+            copy += '\\n';
+            ret += '<br>';
+            continue;
+        }
+        
+        if(char === ' '){
+            copy += ' ';
+            ret += ' ';
+            continue;
+        }
+        
+        let color = "#" +  rainbow.colourAt(i);
+        
+        ret += "<span style='color: " + color + "'>" + char + "</span>";
+        copy += "<color=" + color + ">" + char + "</color>";
+    }
+
+    $("#output").val(copy)
+    
+    let preview = $("#name");
+    
+    preview.empty();
+    preview.append(ret);
+    
+    return ret;
+}
+
+function copyGradient(){
+    navigator.clipboard.writeText($("#output").val()).then(r => {
+        console.log(r)
+        Swal.fire({
+            icon: 'success',
+            title: 'Copied',
+            showConfirmButton: false,
+            timer: 1500
+        })
+    });
+}
